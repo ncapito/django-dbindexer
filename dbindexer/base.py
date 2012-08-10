e869ec607d25c2dc89eeeb4cf9ef2f6db4e3d1f0
Nick Capito <ncapito@gmail.com>
2012-08-09 22:35:47 -0400

    Fixed issue with metaclass conflict

--- dbindexer/base.py
+++ dbindexer/base.py
@@ -39,15 +39,12 @@ class BaseDatabaseWrapper(object):
             pass
         self.ops.__class__ = Operations
         self.ops.__init__()
+from djangoappengine.db.base import DatabaseWrapper as DW
+class Wrapper(BaseDatabaseWrapper, DW):
+    pass
 
 def DatabaseWrapper(settings_dict, *args, **kwargs):
     target_settings = settings.DATABASES[settings_dict['TARGET']]
-    engine = target_settings['ENGINE'] + '.base'
-    target = import_module(engine).DatabaseWrapper
-    class Wrapper(BaseDatabaseWrapper, target):
-        pass
-
     # Update settings with target database settings (which can contain nested dicts).
     merge_dicts(settings_dict, target_settings)
-
     return Wrapper(settings_dict, *args, **kwargs)
